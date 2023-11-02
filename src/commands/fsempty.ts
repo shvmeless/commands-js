2a65d039b017ef3f758e2a15406c8f9e0c7b0f10
Luciano Aedo Puga <81434301+shvmeless@users.noreply.github.com>
2023-11-02 15:25:45 -0300

    feat: add the `fsempty` command
    This command display a list of all empty directories found within the given path.

---
+++ src/commands/fsempty.ts
@@ -0,0 +1,53 @@
+#!/usr/bin/env node
+import { Argument, program } from 'commander'
+import { string as stringScheme } from 'yup'
+import { readdirSync, statSync } from 'fs'
+import { join } from 'path'
+import chalk from 'chalk'
+
+// ARGUMENTS
+const ARGS = {
+  path: new Argument('[path]', 'Initial directory path.').default(process.cwd()),
+}
+
+// MAIN
+program
+  .version('0.0.1')
+  .description('Display a list of all empty directories found within the given path.')
+  .addArgument(ARGS.path)
+  .action(async (argPath) => {
+
+    const targetPath = stringScheme().required().validateSync(argPath)
+
+    const empty: string[] = []
+
+    process.stdout.write('\n')
+
+    const findEmptyDirectories = (path: string): void => {
+
+      const content = readdirSync(path)
+
+      if (content.length === 0) {
+        empty.push(path)
+        process.stdout.write(`${path}\n`)
+      }
+
+      for (const child of content) {
+
+        const childPath = join(path, child)
+        const childStats = statSync(childPath)
+
+        if (childStats.isDirectory()) findEmptyDirectories(childPath)
+
+      }
+
+    }
+
+    findEmptyDirectories(targetPath)
+
+    process.stdout.write(`\n${chalk.yellow.bold(empty.length)} empty directories found!\n\n`)
+
+  })
+
+// RUN
+program.parse()
